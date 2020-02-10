899f5943ed25fffd452ff60c8d6b6b2c3c93c3b5
lpdft <772346730@qq.com>
2020-02-10 13:10:34 +0800

    [ruyue] feat: create Chrome class and override F5 function

--- 006_polymorphism/src/main/java/Application.java
+++ 006_polymorphism/src/main/java/Application.java
@@ -2,5 +2,7 @@ public class Application {
     public static void main(String[] args) {
         Windows intellijIdea = new IntellijIdea();
         intellijIdea.responsePressF5();
+        Windows chrome = new Chrome();
+        chrome.responsePressF5();
     }
 }
---
+++ 006_polymorphism/src/main/java/Chrome.java
@@ -0,0 +1,6 @@
+public class Chrome extends Windows {
+    @Override
+    public void responsePressF5() {
+        System.out.println("触发刷新网页操作");
+    }
+}
