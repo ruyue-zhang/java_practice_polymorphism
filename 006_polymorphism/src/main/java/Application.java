e8f1640ba0a7248163515c0bef254ccb7d019f34
lpdft <772346730@qq.com>
2020-02-10 13:08:22 +0800

    [ruyue] feat: create IntellijIdea class and override F5 function

--- 006_polymorphism/src/main/java/Application.java
+++ 006_polymorphism/src/main/java/Application.java
@@ -1,5 +1,6 @@
 public class Application {
     public static void main(String[] args) {
-
+        Windows intellijIdea = new IntellijIdea();
+        intellijIdea.responsePressF5();
     }
 }
---
+++ 006_polymorphism/src/main/java/IntellijIdea.java
@@ -0,0 +1,6 @@
+public class IntellijIdea extends Windows {
+    @Override
+    public void responsePressF5() {
+        System.out.println("弹出copy当前文件弹窗");
+    }
+}
